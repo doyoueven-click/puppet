a78368920674a624efb135c4229ffed5ec84626b
Amelia Meyer <agmlego@gmail.com>
2023-07-08 18:37:47 -0400

    Add user and group to webroot

--- modules/httpd/manifests/static.pp
+++ modules/httpd/manifests/static.pp
@@ -1,13 +1,20 @@
 # Set up a static site with caddy
 # @param hostname The vhost FQDN
 # @param webroot The directory to use as the webroot
+# @param user The user to own the files
+# @param group The group to own the files
 define httpd::static (
   String $hostname = $title,
   String $webroot,
+  String $user = 'www-data',
+  String $group = 'www-data',
 ) {
   require caddy
   file { $webroot:
     ensure => 'directory',
+    owner  => $user,
+    group  => $group,
+    mode   => 'a+r',
   }
   -> caddy::vhost { $hostname:
     content => "
