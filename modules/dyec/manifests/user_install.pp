7162aec1de6e650676fcd5c72b24c58d2596dfef
Amelia Meyer <agmlego@gmail.com>
2023-07-08 15:53:17 -0400

    Added more must-have programs

--- modules/dyec/manifests/user_install.pp
+++ modules/dyec/manifests/user_install.pp
@@ -1,23 +1,45 @@
 # Sets up essential groups, sudo, and other baseline user stuff.
-class dyec::user_install(
+class dyec::user_install (
   # TODO: Options
 ) {
   package {
     'zsh':
       ensure => 'latest',
-    ;
+      ;
     'sudo':
       ensure => 'latest',
-    ;
+      ;
     'passwd':
       ensure => 'latest',
-    ;
+      ;
     'rsync':
       ensure => 'latest',
+      ;
+    'python3-pip':
+      ensure => 'installed',
+      ;
+    'vim':
+      ensure=> 'installed',
+      ;
+    'git':
+      ensure=> 'installed',
+      ;
+    'screen':
+      ensure=> 'installed',
+      ;
+    'tmux':
+      ensure=> 'installed',
+      ;
+    'di':
+      ensure=> 'installed',
+      ;
+    'htop':
+      ensure=> 'installed',
+      ;
   }
 
   file { '/etc/sudoers.d/rsync':
-    ensure  => 'present',
+    ensure  => 'file',
     content => ' %sudo ALL=(ALL) NOPASSWD:/usr/bin/rsync',
   }
 }
