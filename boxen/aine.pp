623debb07ca450f8682880e1a8a27d374aed09ae
Amelia Meyer <agmlego@gmail.com>
2023-07-08 18:38:28 -0400

    Configure pintailsailing.org and the Factorio dokuwiki on aine

--- boxen/aine.pp
+++ boxen/aine.pp
@@ -1,1 +1,10 @@
 require dyec::common
+httpd::static { 'pintailsailing.org':
+  webroot => '/var/www/pintailsailing.org',
+}
+
+# install dokuwiki for Factorio
+httpd::dokuwiki { 'wiki.sithsite.space':
+  webroot => '/var/www/wiki.sithsite.space',
+  release => 'release-2023-04-04a',
+}
